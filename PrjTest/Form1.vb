0a7140e9e12652efd2db4d14bdd5494332e238bb
HaroldOtsus <haroldotsus@gmail.com>
2023-02-11 22:19:38 +0200

    Loodud windows forms application ja nupud erinevate tujude jaoks. Lisatud nuppudele sündmused.

---
+++ PrjTest/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub btnConfused_Click(sender As Object, e As EventArgs) Handles btnConfused.Click
+        UCtrlEmotion1.setMood("confused")
+    End Sub
+
+    Private Sub btnCool_Click(sender As Object, e As EventArgs) Handles btnCool.Click
+        UCtrlEmotion1.setMood("cool")
+    End Sub
+
+    Private Sub btnSmile_Click(sender As Object, e As EventArgs) Handles btnSmile.Click
+        UCtrlEmotion1.setMood("smile")
+    End Sub
+
+    Private Sub btnStar_Click(sender As Object, e As EventArgs) Handles btnStar.Click
+        UCtrlEmotion1.setMood("star")
+    End Sub
+
+    Private Sub btnTeeth_Click(sender As Object, e As EventArgs) Handles btnTeeth.Click
+        UCtrlEmotion1.setMood("teeth")
+    End Sub
+
+    Private Sub btnYell_Click(sender As Object, e As EventArgs) Handles btnYell.Click
+        UCtrlEmotion1.setMood("yell")
+    End Sub
+End Class
